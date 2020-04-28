74bc495478b405372e5c611d68d7ff0a8157235d
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-04-28 22:35:51 +0200

    firebase collection made for projects, 2 documents so far. forEaching the data from firebase and displaying it on the work page through document.createElement, still need to sort out the internallinkElem Href.

--- src/js/shared.js
+++ src/js/shared.js
@@ -1,5 +1,1 @@
 var matchMobile = window.matchMedia("(max-width:749px)");
-// db.collection('test').add({
-//     title: "Test",
-//     name: "Teeeest"
-// })
---
+++ src/js/work.js
@@ -0,0 +1,58 @@
+let workProjectWrapperElem = document.querySelector('.work__project-wrapper');
+
+db.collection('projects').get() // uden doc() hvis du skal hente det hele, husk at forEach.
+    .then((projects)=>{
+        projects.forEach((project)=>{
+            console.log('project: ', project.data());
+            console.log('project: ', project.id);
+
+            let workProjectContainerElem = document.createElement('div');
+            let workBackgroundImageElem = document.createElement('img');
+            let workProjectTitleElem = document.createElement('h2');
+            let workLinkContainerElem = document.createElement('div');
+            let workGithubLinkElem = document.createElement('a');
+            let workDomainLinkElem = document.createElement('a');
+            let workInternalLinkElem = document.createElement('a');
+
+            workProjectContainerElem.classList.add('work__project-container');
+            workBackgroundImageElem.classList.add('work__background-image');
+            workProjectTitleElem.classList.add('work__project-title');
+            workLinkContainerElem.classList.add('work__link-container');
+            workGithubLinkElem.classList.add('work__link');
+            workDomainLinkElem.classList.add('work__link');
+            workInternalLinkElem.classList.add('work__link');
+
+            workBackgroundImageElem.src = project.data().image;
+            workProjectTitleElem.innerHTML = project.data().title;
+            workGithubLinkElem.href = project.data().githubRepo;
+            workGithubLinkElem.innerHTML = "Github Repo";
+            workGithubLinkElem.target = "_blank";
+            workDomainLinkElem.href = project.data().domain;
+            workDomainLinkElem.target = "_blank";
+            workDomainLinkElem.innerHTML = "Visit Domain";
+            workInternalLinkElem.href = `#`;
+            workInternalLinkElem.innerHTML = "View Project";
+
+            workProjectWrapperElem.appendChild(workProjectContainerElem);
+            workProjectContainerElem.appendChild(workBackgroundImageElem);
+            workProjectContainerElem.appendChild(workProjectTitleElem);
+            workProjectContainerElem.appendChild(workLinkContainerElem);
+            workLinkContainerElem.appendChild(workGithubLinkElem);
+            workLinkContainerElem.appendChild(workDomainLinkElem);
+            workProjectContainerElem.appendChild(workInternalLinkElem);
+
+
+
+
+
+          
+        })
+    })
+
+
+    
+// db.collection('users').doc('W2C8Q9RbRh71nCX2KFTN').get() // doc() hvis du kun skal hente et specifikt dokument.
+//     .then((users)=>{
+//     console.log('users: ', users.data());
+        
+//     })
