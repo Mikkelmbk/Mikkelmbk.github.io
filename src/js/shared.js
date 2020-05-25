3c1be750a2022ef00de1eb8a2bc977a66672a589
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-05-25 21:47:33 +0200

    added section to employment history, added a To-the-top button when the user is further than 200 pixels from the top

--- src/js/shared.js
+++ src/js/shared.js
@@ -1,1 +1,15 @@
 var matchMobile = window.matchMedia("(max-width:749px)");
+
+var toTop = document.querySelector('.wrapper__span-to-top');
+document.addEventListener('scroll', () => {
+    var scrollPos = -50;
+    if (document.body.getBoundingClientRect().top < scrollPos) {
+        toTop.style.opacity = 0.75;
+        toTop.style.pointerEvents = "auto";
+
+    }
+    else {
+        toTop.style.opacity = 0;
+        toTop.style.pointerEvents = "none";
+    }
+});
