0faa527f8964067f1df3c6c1ae857054d35e2673
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-06-29 15:02:32 +0200

    worked out the HTML for the single project view display and added the data for that specific page to the firebase documents

--- src/js/project.js
+++ src/js/project.js
@@ -1,4 +1,6 @@
 let URLparams = new URLSearchParams(window.location.search);
+let projectWrapperInnerElem = document.querySelector('.project__wrapper-inner');
+console.log('projectWrapperInnerElem: ', projectWrapperInnerElem);
 
 
 
@@ -9,10 +11,30 @@ if (URLparams.get('id') != null || URLparams.get('id') != "") {
             return res.json()
         })
         .then((project) => {
+            console.log('project: ', project);
             let projectTitleElem = document.querySelector('.project__title');
+            let projectTeamContentElem = document.querySelector('.project__team-content');
+            let projectGoalContentElem = document.querySelector('.project__goal-content');
+            let projectDurationContentElem = document.querySelector('.project__duration-content');
+            let projectAboutContentElem = document.querySelector('.project__about-content');
+            let projectTechnologiesContentElem = document.querySelector('.project__technologies-content');
+            let projectChallengesContentElem = document.querySelector('.project__challenges-content');
+            let projectProcessContentElem = document.querySelector('.project__process-content');
+        
+
+
             projectTitleElem.innerHTML = project.title;
+            projectTeamContentElem.innerHTML = project.details.team;
+            projectGoalContentElem.innerHTML = project.details.goal;
+            projectDurationContentElem.innerHTML = project.details.duration;
+            projectAboutContentElem.innerHTML = project.details.about;
+            projectTechnologiesContentElem.innerHTML = project.details.technologies;
+            projectChallengesContentElem.innerHTML = project.details.challenges;
+            projectProcessContentElem.innerHTML = project.details.process;
+            
 
 
             loadingOverlayElem.classList.add('display-none');
+            projectWrapperInnerElem.classList.remove('invisible');
         })
 }
