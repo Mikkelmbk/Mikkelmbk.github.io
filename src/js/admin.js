f6d177c2d533081e579a089eb500e696bc4fe888
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-07-04 14:49:46 +0200

    working on the update system, trying to make it as automated as possible, building the form and the correct amount of input fields depending on how many properties are within the chosen product or skill to update

--- src/js/admin.js
+++ src/js/admin.js
@@ -5,7 +5,7 @@ fetch('https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/authenticate
     }
 })
     .then((data) => {
-        console.log('data: ', data);
+        // console.log('data: ', data);
         if (data.status === 401) {
             window.location.replace(`${window.location.origin}/admin-login/`);
         }
@@ -16,7 +16,7 @@ fetch('https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/authenticate
 
 
 window.addEventListener('hashchange', (e) => {
-    console.log(e);
+    // console.log(e);
     let collectionName = e.newURL.split('#!/')[1];
 
     fetch(`https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/${collectionName}`)
@@ -25,15 +25,16 @@ window.addEventListener('hashchange', (e) => {
         })
         .then((items) => {
             let adminTableContainerElem = document.querySelector('.admin__table-container');
+            let adminFormContainerElem = document.querySelector('.admin__form-container');
             adminTableContainerElem.innerHTML = "";
             let adminTableElem = document.createElement('table');
-            adminTableElem.classList.add('admin-table');
+            adminTableElem.classList.add('admin__table');
             adminTableElem.innerHTML = `
             <tr><th>Actions</th><th>Title</th></tr>
             `;
             adminTableContainerElem.appendChild(adminTableElem);
 
-            console.log('items: ', items);
+            // console.log('items: ', items);
 
             items.forEach((item) => {
 
@@ -44,11 +45,12 @@ window.addEventListener('hashchange', (e) => {
                 let adminTitle = document.createElement('td');
 
 
-                adminBtnContainer.classList.add('gallery-btn-container');
+                adminBtnContainer.classList.add('admin__btn-container');
                 adminUpdate.innerHTML = "Update";
                 adminUpdate.dataset.id = item.docId;
                 adminDelete.innerHTML = "Delete";
                 adminDelete.dataset.id = item.docId;
+                adminDelete.dataset.title = item.title;
                 adminTitle.innerHTML = item.title;
 
 
@@ -59,14 +61,50 @@ window.addEventListener('hashchange', (e) => {
                 adminTr.appendChild(adminTitle);
 
                 adminUpdate.addEventListener('click', (e) => {
-                    console.log('e: ', e);
+                    adminFormContainerElem.innerHTML = "";
+                    // console.log('e: ', e);
+                    let itemToUpdate = items.find(item => item.docId === e.target.dataset.id);
+                    // console.log('itemToUpdate: ', itemToUpdate);
+
+                    let adminFormElem = document.createElement('form');
+                    adminFormElem.classList.add('admin__form');
+                    adminFormContainerElem.appendChild(adminFormElem);
+
+
+                    let properties = Object.entries(itemToUpdate);
+                    // console.log('properties: ', properties);
+                    
+                    if (itemToUpdate.details) {
+                        properties = [...properties, ...Object.entries(itemToUpdate.details)];
+                        
+                        properties.splice(properties.findIndex(property => property[0] === "details"), 1) // finds the index of the entry with the property name details, and uses that index to splice.  
+                    }
+                    properties.splice(properties.findIndex(property => property[0] === "docId"), 1);
+                    console.log('properties AFTER: ', properties);
+
+
+                    properties.forEach((property)=>{
+                    console.log('property: ', property);
+                        
+                    })
+
+
+
+
+
+
+
+
+
 
                 })
 
                 adminDelete.addEventListener('click', (e) => {
                     console.log('e: ', e);
 
-                    if (confirm(`Are you sure you want to delete this item from the ${collectionName.toUpperCase()} list`)) {
+
+
+                    if (confirm(`Are you sure you want to delete ${e.target.dataset.title} from the ${collectionName.toUpperCase()} list`)) {
                         fetch(`https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/delete-${collectionName}/${e.target.dataset.id}`, {
                             "method": "DELETE",
                             "headers": {
@@ -88,7 +126,7 @@ window.addEventListener('hashchange', (e) => {
             let adminAddBtnContainer = document.createElement('td');
             let adminAdd = document.createElement('button');
 
-            adminAddBtnContainer.classList.add('gallery-btn-container');
+            adminAddBtnContainer.classList.add('admin__btn-container');
             adminAdd.innerHTML = `Add ${collectionName}`;
 
             adminTableElem.appendChild(adminAddTr);
@@ -97,7 +135,7 @@ window.addEventListener('hashchange', (e) => {
 
 
             adminAdd.addEventListener('click', (e) => {
-            console.log('e: ', e);
+                console.log('e: ', e);
 
             });
 
@@ -108,10 +146,109 @@ window.addEventListener('hashchange', (e) => {
 
         })
         .catch((err) => {
-        console.log('err: ', err);
+            console.log('err: ', err);
         })
 
 
 
 })
+
+
+// if(collectionName === "projects"){
+
+//     let adminDomainLabel = document.createElement('label');
+//     let adminDomainInput = document.createElement('input');
+
+//     let adminGithubRepoLabel = document.createElement('label');
+//     let adminGithubRepoInput = document.createElement('input');
+
+//     let adminImageLabel = document.createElement('label');
+//     let adminImageInput = document.createElement('input');
+
+//     let adminTitleLabel = document.createElement('label');
+//     let adminTitleInput = document.createElement('input');
+
+//     let adminAboutLabel = document.createElement('label');
+//     let adminAboutTextarea = document.createElement('textarea');
+
+//     let adminChallengesLabel = document.createElement('label');
+//     let adminChallengesTextarea = document.createElement('textarea');
+
+//     let adminDurationLabel = document.createElement('label');
+//     let adminDurationTextarea = document.createElement('textarea');
+
+//     let adminGoalLabel = document.createElement('label');
+//     let adminGoalTextarea = document.createElement('textarea');
+
+//     let adminProcessLabel = document.createElement('label');
+//     let adminProcessTextarea = document.createElement('textarea');
+
+//     let adminTeamLabel = document.createElement('label');
+//     let adminTeamTextarea = document.createElement('textarea');
+
+//     let adminTechnologiesLabel = document.createElement('label');
+//     let adminTechnologiesTextarea = document.createElement('textarea');
+
+//     let adminSendUpdateBtn = document.createElement('button');
+
+//     adminDomainLabel.innerHTML = "Domain"
+//     adminDomainInput.value = itemToUpdate.domain;
+
+//     adminGithubRepoInput.value = itemToUpdate.githubRepo;
+//     adminImageInput.value = itemToUpdate.image.split('/img/')[1];
+//     adminTitleInput.value = itemToUpdate.title;
+//     adminAboutTextarea.value = itemToUpdate.details.about;
+//     adminChallengesTextarea.value = itemToUpdate.details.challenges;
+//     adminDurationTextarea.value = itemToUpdate.details.duration;
+//     adminGoalTextarea.value = itemToUpdate.details.goal;
+//     adminProcessTextarea.value = itemToUpdate.details.process;
+//     adminTeamTextarea.value = itemToUpdate.details.team;
+//     adminTechnologiesTextarea.value = itemToUpdate.details.technologies;
+//     adminSendUpdateBtn.innerHTML = `Update ${itemToUpdate.title}`;
+
+
+
+//     adminFormElem.appendChild(adminDomainLabel);
+//     adminFormElem.appendChild(adminDomainInput);
+
+//     adminFormElem.appendChild(adminGithubRepoLabel);
+//     adminFormElem.appendChild(adminGithubRepoInput);
+
+//     adminFormElem.appendChild(adminImageLabel);
+//     adminFormElem.appendChild(adminImageInput);
+
+//     adminFormElem.appendChild(adminTitleLabel);
+//     adminFormElem.appendChild(adminTitleInput);
+
+//     adminFormElem.appendChild(adminAboutLabel);
+//     adminFormElem.appendChild(adminAboutTextarea);
+
+//     adminFormElem.appendChild(adminChallengesLabel);
+//     adminFormElem.appendChild(adminChallengesTextarea);
+
+//     adminFormElem.appendChild(adminDurationLabel);
+//     adminFormElem.appendChild(adminDurationTextarea);
+
+//     adminFormElem.appendChild(adminGoalLabel);
+//     adminFormElem.appendChild(adminGoalTextarea);
+
+//     adminFormElem.appendChild(adminProcessLabel);
+//     adminFormElem.appendChild(adminProcessTextarea);
+
+//     adminFormElem.appendChild(adminTeamLabel);
+//     adminFormElem.appendChild(adminTeamTextarea);
+
+//     adminFormElem.appendChild(adminTechnologiesLabel);
+//     adminFormElem.appendChild(adminTechnologiesTextarea);
+
+//     adminFormElem.appendChild(adminSendUpdateBtn);
+
+
+
+// }
+
+
+// if(collectionName === "skills"){
+
+// }
 
