a10473bd34965160753968f1743112c08c48566c
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-07-06 13:44:39 +0200

    I now have a full CRUD on my admin panel, managing both projects and skills

--- src/js/admin.js
+++ src/js/admin.js
@@ -120,7 +120,7 @@ window.addEventListener('hashchange', (e) => {
 
                             validateInputElems.forEach((validateInputElem) => {
                                 validateInputElem.style.backgroundColor = "rgba(150, 150, 150, 0.226)";
-                                if (validateInputElem.value.trim() == "") {
+                                if (validateInputElem.value.trim() == "") { // trim to remove whitespace so that the value is infact empty when no words, numbers or symbols are typed.
                                     validate = false;
                                     validateInputElem.style.backgroundColor = "red";
                                 }
@@ -130,7 +130,7 @@ window.addEventListener('hashchange', (e) => {
 
 
                                 fetch(`https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/update-${collectionName}/${itemToUpdate.docId}`, {
-                                    "method":"POST",
+                                    "method": "POST",
                                     "headers": {
                                         "Authorization": `Bearer ${sessionStorage.getItem('idToken')}`,
                                         "Content-Type": "application/x-www-form-urlencoded"
@@ -147,16 +147,16 @@ window.addEventListener('hashchange', (e) => {
                                     &team=${adminFormElem.team.value.trim()}
                                     &technologies=${adminFormElem.technologies.value.trim()}`
                                 })
-                                .then(()=>{
-                                    window.location.replace(`${window.location.origin}/admin/`);
-                                })
+                                    .then(() => {
+                                        window.location.replace(`${window.location.origin}/admin/`);
+                                    })
 
                             }
 
                             if (collectionName === "skills" && validate) {
 
                                 fetch(`https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/update-${collectionName}/${itemToUpdate.docId}`, {
-                                    "method":"POST",
+                                    "method": "POST",
                                     "headers": {
                                         "Authorization": `Bearer ${sessionStorage.getItem('idToken')}`,
                                         "Content-Type": "application/x-www-form-urlencoded"
@@ -165,9 +165,9 @@ window.addEventListener('hashchange', (e) => {
                                     &order=${adminFormElem.order.value.trim()}
                                     `
                                 })
-                                .then(()=>{
-                                    window.location.replace(`${window.location.origin}/admin/`);
-                                })
+                                    .then(() => {
+                                        window.location.replace(`${window.location.origin}/admin/`);
+                                    })
 
 
 
@@ -175,17 +175,6 @@ window.addEventListener('hashchange', (e) => {
                         }
 
                     })
-
-
-
-
-
-
-
-
-
-
-
 
 
                 })
@@ -226,14 +215,241 @@ window.addEventListener('hashchange', (e) => {
 
 
             adminAdd.addEventListener('click', (e) => {
-                console.log('e: ', e);
+
+                adminFormContainerElem.innerHTML = "";
+
+                let adminFormElem = document.createElement('form');
+                adminFormContainerElem.appendChild(adminFormElem);
+
+                adminFormElem.classList.add('admin__form');
+
+
+                if (collectionName === "projects") {
+
+                    let adminDomainLabel = document.createElement('label');
+                    let adminDomainInput = document.createElement('input');
+
+                    let adminGithubRepoLabel = document.createElement('label');
+                    let adminGithubRepoInput = document.createElement('input');
+
+                    let adminImageLabel = document.createElement('label');
+                    let adminImageInput = document.createElement('input');
+
+                    let adminTitleLabel = document.createElement('label');
+                    let adminTitleInput = document.createElement('input');
+
+                    let adminAboutLabel = document.createElement('label');
+                    let adminAboutTextarea = document.createElement('textarea');
+
+                    let adminChallengesLabel = document.createElement('label');
+                    let adminChallengesTextarea = document.createElement('textarea');
+
+                    let adminDurationLabel = document.createElement('label');
+                    let adminDurationTextarea = document.createElement('textarea');
+
+                    let adminGoalLabel = document.createElement('label');
+                    let adminGoalTextarea = document.createElement('textarea');
+
+                    let adminProcessLabel = document.createElement('label');
+                    let adminProcessTextarea = document.createElement('textarea');
+
+                    let adminTeamLabel = document.createElement('label');
+                    let adminTeamTextarea = document.createElement('textarea');
+
+                    let adminTechnologiesLabel = document.createElement('label');
+                    let adminTechnologiesTextarea = document.createElement('textarea');
+
+                    let adminAddBtn = document.createElement('button');
+
+
+                    adminAddBtn.innerHTML = "Add Project";
+                    adminDomainLabel.innerHTML = "Domain";
+                    adminGithubRepoLabel.innerHTML = "GithubRepo";
+                    adminImageLabel.innerHTML = "Image";
+                    adminTitleLabel.innerHTML = "Title";
+                    adminAboutLabel.innerHTML = "About";
+                    adminChallengesLabel.innerHTML = "Challenges";
+                    adminDurationLabel.innerHTML = "Duration";
+                    adminGoalLabel.innerHTML = "Goal";
+                    adminProcessLabel.innerHTML = "Process";
+                    adminTeamLabel.innerHTML = "Team";
+                    adminTechnologiesLabel.innerHTML = "Technologies";
+
+
+                    adminDomainInput.name = "domain";
+                    adminGithubRepoInput.name = "githubRepo";
+                    adminImageInput.name = "image";
+                    adminTitleInput.name = "title";
+                    adminAboutTextarea.name = "about";
+                    adminChallengesTextarea.name = "challenges";
+                    adminDurationTextarea.name = "duration";
+                    adminGoalTextarea.name = "goal";
+                    adminProcessTextarea.name = "process";
+                    adminTeamTextarea.name = "team";
+                    adminTechnologiesTextarea.name = "technologies";
+
+
+                    adminFormElem.appendChild(adminDomainLabel);
+                    adminFormElem.appendChild(adminDomainInput);
+
+                    adminFormElem.appendChild(adminGithubRepoLabel);
+                    adminFormElem.appendChild(adminGithubRepoInput);
+
+                    adminFormElem.appendChild(adminImageLabel);
+                    adminFormElem.appendChild(adminImageInput);
+
+                    adminFormElem.appendChild(adminTitleLabel);
+                    adminFormElem.appendChild(adminTitleInput);
+
+                    adminFormElem.appendChild(adminAboutLabel);
+                    adminFormElem.appendChild(adminAboutTextarea);
+
+                    adminFormElem.appendChild(adminChallengesLabel);
+                    adminFormElem.appendChild(adminChallengesTextarea);
+
+                    adminFormElem.appendChild(adminDurationLabel);
+                    adminFormElem.appendChild(adminDurationTextarea);
+
+                    adminFormElem.appendChild(adminGoalLabel);
+                    adminFormElem.appendChild(adminGoalTextarea);
+
+                    adminFormElem.appendChild(adminProcessLabel);
+                    adminFormElem.appendChild(adminProcessTextarea);
+
+                    adminFormElem.appendChild(adminTeamLabel);
+                    adminFormElem.appendChild(adminTeamTextarea);
+
+                    adminFormElem.appendChild(adminTechnologiesLabel);
+                    adminFormElem.appendChild(adminTechnologiesTextarea);
+
+                    adminFormElem.appendChild(adminAddBtn);
+
+
+                    adminFormElem.addEventListener('submit', (e) => {
+                        e.preventDefault();
+                        let validate = true;
+                        let tempElems = [...adminFormElem.children]; // change from a HTMLcollection, to an ordinary array using the spread operator, this way i can use forEach, filter etc on the array.
+        
+
+                        let elemsToValidate = tempElems.filter((tempElem) => { // remove all the labels and buttons from the array, this way i can forEach on the array which only contains textAreas and Inputs, and validate their inputs.
+                            if (tempElem.nodeName !== "LABEL" && tempElem.nodeName !== "BUTTON") {
+                                return tempElem;
+                            }
+                        });
+
+                        elemsToValidate.forEach((elemToValidate) => {
+                            elemToValidate.style.backgroundColor = "rgba(150, 150, 150, 0.226)";
+                            if (elemToValidate.value.trim() == "") { // trim to remove whitespace so that the value is infact empty when no words, numbers or symbols are typed.
+                                validate = false;
+                                elemToValidate.style.backgroundColor = "red";
+                            }
+                        });
+
+
+                        if(validate){
+
+                            fetch(`https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/add-${collectionName}`, {
+                                "method":"POST",
+                                "headers": {
+                                    "Authorization": `Bearer ${sessionStorage.getItem('idToken')}`,
+                                    "Content-Type": "application/x-www-form-urlencoded"
+                                },
+                                "body": `domain=${adminFormElem.domain.value.trim()}
+                                    &githubRepo=${adminFormElem.githubRepo.value.trim()}
+                                    &image=${adminFormElem.image.value.trim()}
+                                    &title=${adminFormElem.title.value.trim()}
+                                    &about=${adminFormElem.about.value.trim()}
+                                    &challenges=${adminFormElem.challenges.value.trim()}
+                                    &duration=${adminFormElem.duration.value.trim()}
+                                    &goal=${adminFormElem.goal.value.trim()}
+                                    &process=${adminFormElem.process.value.trim()}
+                                    &team=${adminFormElem.team.value.trim()}
+                                    &technologies=${adminFormElem.technologies.value.trim()}`
+                            })
+                            .then(()=>{
+                                window.location.replace(`${window.location.origin}/admin/`);
+                            })
+
+                        }
+
+                    })
+
+                }
+
+
+                if (collectionName === "skills") {
+                    let adminOrderLabel = document.createElement('label');
+                    let adminOrderInput = document.createElement('input');
+
+                    let adminTitleLabel = document.createElement('label');
+                    let adminTitleInput = document.createElement('input');
+
+
+                    let adminAddBtn = document.createElement('button');
+
+
+                    adminAddBtn.innerHTML = "Add Skills";
+                    adminOrderLabel.innerHTML = "Order";
+                    adminTitleLabel.innerHTML = "Title";
+
+
+                    adminOrderInput.name = "order";
+                    adminTitleInput.name = "title";
+
+
+                    adminFormElem.appendChild(adminOrderLabel);
+                    adminFormElem.appendChild(adminOrderInput);
+
+                    adminFormElem.appendChild(adminTitleLabel);
+                    adminFormElem.appendChild(adminTitleInput);
+
+                    adminFormElem.appendChild(adminAddBtn);
+
+
+                    adminFormElem.addEventListener('submit', (e) => {
+                        e.preventDefault();
+                        let validate = true;
+                        let tempElems = [...adminFormElem.children]; // change from a HTMLcollection, to an ordinary array using the spread operator, this way i can use forEach, filter etc on the array.
+        
+
+                        let elemsToValidate = tempElems.filter((tempElem) => { // remove all the labels and buttons from the array, this way i can forEach on the array which only contains textAreas and Inputs, and validate their inputs.
+                            if (tempElem.nodeName !== "LABEL" && tempElem.nodeName !== "BUTTON") {
+                                return tempElem;
+                            }
+                        });
+
+                        elemsToValidate.forEach((elemToValidate) => {
+                            elemToValidate.style.backgroundColor = "rgba(150, 150, 150, 0.226)";
+                            if (elemToValidate.value.trim() == "") { // trim to remove whitespace so that the value is infact empty when no words, numbers or symbols are typed.
+                                validate = false;
+                                elemToValidate.style.backgroundColor = "red";
+                            }
+                        });
+
+
+                        if(validate){
+
+                            fetch(`https://us-central1-mbk-portfolio.cloudfunctions.net/app/api/add-${collectionName}`, {
+                                "method":"POST",
+                                "headers": {
+                                    "Authorization": `Bearer ${sessionStorage.getItem('idToken')}`,
+                                    "Content-Type": "application/x-www-form-urlencoded"
+                                },
+                                "body": `order=${adminFormElem.order.value.trim()}
+                                    &title=${adminFormElem.title.value.trim()}
+                                    `
+                            })
+                            .then(()=>{
+                                window.location.replace(`${window.location.origin}/admin/`);
+                            })
+
+                        }
+
+                    })
+
+                }
 
             });
-
-
-
-
-
 
         })
         .catch((err) => {
